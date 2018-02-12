b6db9e16dbb2a54966e650760613dfc49cfe1b82
baijunyao <baijunyao@baijunyao.com>
2018-02-12 10:48:54 +0800

    处理手机号错误或者未查询到数据的情况

--- src/Phone.php
+++ src/Phone.php
@@ -7,12 +7,32 @@ namespace Baijunyao\Phone;
 */
 class Phone
 {
+    /**
+     * 获取手机号服务商以及归属地
+     *
+     * @param int $phone
+     *
+     * @return array|mixed
+     */
     public static function find(int $phone)
     {
         $url = 'https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel='.$phone."&t=".time();
         $gbkStr = file_get_contents($url);
         $utf8Str = iconv("GBK", "UTF-8", $gbkStr);
-        $data = preg_replace(["/__GetZoneResult_ = /", "/([a-zA-Z_]+[a-zA-Z0-9_]*)\s*:/", "/:\s*'(.*?)'/"], ['', '"\1":', ': "\1"'], $utf8Str);
-        return json_decode($data, true);
+        $jsonStr = preg_replace(["/__GetZoneResult_ = /", "/([a-zA-Z_]+[a-zA-Z0-9_]*)\s*:/", "/:\s*'(.*?)'/"], ['', '"\1":', ': "\1"'], $utf8Str);
+        $data = json_decode($jsonStr, true);
+        // 如果手机号有误 或者其他情况未查出数据 则全赋值为空
+        if (empty($data['province'])) {
+            $data = [
+                'mts' => '',
+                'province' => '',
+                'catName' => '',
+                'telString' => '',
+                'areaVid' => '',
+                'ispVid' => '',
+                'carrier' => ''
+            ];
+        }
+        return $data;
     }
 }
